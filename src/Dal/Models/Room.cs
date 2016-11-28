ec57f05e462dd788a0759abbe25ce2322a153e84
Evan Schallerer <schallee@oregonstate.edu>
2016-11-28 11:17:35 -0800

    Added model to get started

---
+++ src/Dal/Models/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dal.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        // TODO: Add Department models
+        //public List<Department> Departments { get; set; }
+    }
+}
