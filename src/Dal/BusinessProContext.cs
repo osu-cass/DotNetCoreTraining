e74e411651ff811de190a0df099db8b5d2901216
Evan Schallerer <schallee@oregonstate.edu>
2016-11-28 11:30:25 -0800

    working on db creation

---
+++ src/Dal/BusinessProContext.cs
@@ -0,0 +1,20 @@
+﻿using Dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal
+{
+    public class BusinessProContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./Blogging.db");
+        }
+    }
+}
